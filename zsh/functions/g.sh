46b45bef8318ee8a38e32dc24e6249e389b14afd
Lee Dohm <lee@liftedstudios.com>
2014-11-06 07:18:41 -0800

    Update to work with `hub` again

--- zsh/functions/g.sh
+++ zsh/functions/g.sh
@@ -2,13 +2,24 @@
 
 # No arguments: `git status`
 # With arguments: acts like `git`
-g() {
-    if [[ $# -gt 0 ]]; then
-        git "$@"
-    else
-        git st
-    fi
-}
+# Creates equivalent versions whether hub is installed or not
+if hash hub 2>/dev/null; then
+    g() {
+        if [[ $# -gt 0 ]]; then
+            hub "$@"
+        else
+            hub st
+        fi
+    }
+else
+    g() {
+        if [[ $# -gt 0 ]]; then
+            git "$@"
+        else
+            git st
+        fi
+    }
+fi
 
 # Complete g like git
 compdef g=git
