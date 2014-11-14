2dcd56c94f17247aa5c26c5ffa05f79d093eea90
Lee Dohm <lee@lee-dohm.com>
2014-11-13 22:08:12 -0800

    Fix ShellCheck warnings

--- host-c82a14544e88/zsh/init/local-only.sh
+++ host-c82a14544e88/zsh/init/local-only.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 local_only="$HOME/Source/local-only"
 local_only_bin="$local_only/bin"
 
-add_to_path $local_only $local_only_bin
+add_to_path "$local_only" "$local_only_bin"
 
 # Perform any local-specific initializations
 for init in $local_only/init/*; do
