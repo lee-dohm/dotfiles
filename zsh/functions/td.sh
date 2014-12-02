b48fd64fe51da1d476daa2cd94addc143d0f2b6f
Lee Dohm <lee@liftedstudios.com>
2014-12-02 12:40:37 -0800

    Add new function for displaying default todo list

--- zsh/functions/td.sh
+++ zsh/functions/td.sh
@@ -1,11 +1,27 @@
 #!/bin/zsh
 
+# Simplified shortcut for `todo.sh`.
+#
+# When:
+# * Given no arguments, executes `todo.sh ls`
+# * Given one or more arguments, executes `todo.sh "$@"`
 td() {
     if [[ $# -gt 0 ]]; then
         todo.sh "$@"
     else
         clear
         todo.sh ls
+    fi
+}
+
+# Displays the default list of todo items for the current location based on `$HOSTNAME`
+tt() {
+    clear
+
+    if [[ "$HOSTNAME" == "c82a14544e88" ]]; then
+        todo.sh ls @Work
+    else
+        todo.sh ls -@Work
     fi
 }
 
