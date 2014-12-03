0df903de8d4eb34c12efb65692bea9f72be21d1c
Lee Dohm <lee@lee-dohm.com>
2014-12-02 21:58:05 -0800

    :lipstick:

--- zsh/functions/envup.sh
+++ zsh/functions/envup.sh
@@ -1,8 +1,9 @@
+#!/bin/zsh
 # Load .env file into shell session for environment variables
 
 function envup() {
   if [ -f .env ]; then
-    export `cat .env`
+    export $(cat .env)
   else
     echo 'No .env file found' 1>&2
     return 1
---
+++ zsh/functions/pman.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# Opens a man page as a PDF in OS X Preview.app
+
+if [[ $(uname) == 'Darwin' ]]; then
+    pman() {
+        man -t "$@" | open -f -a Preview;
+    }
+fi
