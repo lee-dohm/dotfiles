7ff6d89d8aa005154c0873e0de31ec589b251c5e
Lee Dohm <lee@liftedstudios.com>
2014-12-17 12:28:51 -0800

    Add environment variables for proper Android operation

--- zsh/init/android.sh
+++ zsh/init/android.sh
@@ -4,3 +4,6 @@ android="$HOME/android-sdk-macosx"
 android_path="$HOME/android-sdk-macosx/tools:$HOME/android-sdk-macosx/platform-tools"
 
 add_to_path "$android" "$android_path"
+
+export ANDROID_HOME="$android"
+export JAVA_HOME="$(/usr/libexec/java_home)"
