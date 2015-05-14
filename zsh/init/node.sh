78ba3bb22250576244b44d690cdd0753e8532bf0
Lee Dohm <lee@lee-dohm.com>
2015-05-13 19:06:56 -0700

    Fix broken NVM_DIR environment variable

--- zsh/init/node.sh
+++ zsh/init/node.sh
@@ -1,9 +1,9 @@
-#!/bin/zsh
+#!/bin/bash
 
 if [[ $(uname) == "Linux" ]]; then
     export PATH="$PATH:$HOME/npm/bin"
 else
-    export NVM_DIR="~/.nvm"
+    export NVM_DIR="$HOME/.nvm"
     export PATH="$PATH:/usr/local/lib/node_modules"
     source "$(brew --prefix nvm)/nvm.sh"
 fi
