c6b473aab912bfd18c50b4909bb28f48381d6402
Lee Dohm <lee-dohm@github.com>
2016-02-24 12:29:46 -0800

    Fix Ruby environment loading
    For some reason the Ruby environment loading stopped working. This fixes
    it. I'm not sure why :unamused:

--- zsh/init/ruby.sh
+++ zsh/init/ruby.sh
@@ -1,11 +1,9 @@
 #!/bin/zsh
 
 # Ensure rbenv gets loaded if it is available
-if ! hash rbenv 2> /dev/null ; then
-    if [[ -d $HOME/.rbenv ]]; then
-        export PATH=$PATH:$HOME/.rbenv/bin
-        eval "$(rbenv init - --no-rehash)"
-    fi
+if [[ -d $HOME/.rbenv ]]; then
+    export PATH=$PATH:$HOME/.rbenv/bin
+    eval "$(rbenv init - --no-rehash)"
 fi
 
 alias b='bundle'
