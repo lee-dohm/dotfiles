ad71cc04ae595d5217f1743a7d98832c3dbdd360
Lee Dohm <lee@lee-dohm.com>
2016-06-13 13:35:27 -0700

    Switch from nvm to nodenv

--- zsh/init/node.sh
+++ zsh/init/node.sh
@@ -3,7 +3,6 @@
 if [[ $(uname) == "Linux" ]]; then
     export PATH="$PATH:$HOME/npm/bin"
 else
-    export NVM_DIR="$HOME/.nvm"
+    eval "$(nodenv init -)"
     export PATH="$PATH:/usr/local/lib/node_modules"
-    source "$(brew --prefix nvm)/nvm.sh"
 fi
