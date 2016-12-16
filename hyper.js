b2329c352b26a744c2e4698220df402ea9e2a698
Lee Dohm <lee@lee-dohm.com>
2016-12-16 10:38:31 -0800

    Add hyperterm-adventurous theme to Hyper environment

--- hyper.js
+++ hyper.js
@@ -81,7 +81,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [],
+  plugins: ["hyperterm-adventurous"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
