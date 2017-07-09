ea9a1f6e0babc2e11bc48def865ffbc646cc9b48
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-07-09 11:32:04 -0700

    Add Anka/Coder to list of fonts to use in Hyper

--- hyper.js
+++ hyper.js
@@ -4,7 +4,7 @@ module.exports = {
     fontSize: 16,
 
     // font family with optional fallbacks
-    fontFamily: '"Anonymous Pro", "Monaco", "Source Code Pro", "Input Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"Anka/Coder", "Anonymous Pro", "Monaco", "Source Code Pro", "Input Mono", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
