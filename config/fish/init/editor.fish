ffb03ee268a9dae982e8e809af3a00e38e935da2
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-03-10 08:40:21 -0800

    Add aliases to launch different versions of Atom

--- config/fish/init/editor.fish
+++ config/fish/init/editor.fish
@@ -3,3 +3,8 @@ set --export VISUAL $EDITOR
 
 set --export ATOM_DEV_RESOURCE_PATH ~/Source/atom/atom
 set --export ATOM_REPOS_HOME ~/Source
+
+alias a='atom'
+alias aa='atom-dev'
+alias ab='atom-beta'
+alias ad='atom-dev'
