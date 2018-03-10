b20445d06d0fa01af2f68d04e138807f7f7795f1
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-03-10 09:48:05 -0800

    Only run local node modules binaries in bin-safe directories

--- config/fish/init/node.fish
+++ config/fish/init/node.fish
@@ -1,3 +1,3 @@
 status --is-interactive; and source (nodenv init -|psub)
 
-set --export PATH $PATH /usr/local/lib/node_modules ./node_modules/bin
+set --export PATH $PATH /usr/local/lib/node_modules
--- config/fish/init/zz_git_safe.fish
+++ config/fish/init/zz_git_safe.fish
@@ -1,1 +1,2 @@
+set --export PATH .git/bin-safe/../../node_modules/bin $PATH
 set --export PATH .git/bin-safe/../../bin $PATH
