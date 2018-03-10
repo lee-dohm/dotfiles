73756ec3c5d02b6b1958106664aacfdcfacfe325
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-03-09 22:15:54 -0800

    Prevent shortening of prompt pwd components

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -14,6 +14,8 @@ if status --is-interactive
   abbr --add p 'project'
 end
 
+set --global fish_prompt_pwd_dir_length 0
+
 for init in (ls ~/.config/fish/init)
   source ~/.config/fish/init/$init
 end
