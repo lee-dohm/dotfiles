e04040691e0c629b9d52f06921069b8d6e2af40f
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-03-13 11:55:48 -0700

    Add check for REPO_HOME environment variable

--- config/fish/functions/project.fish
+++ config/fish/functions/project.fish
@@ -1,6 +1,11 @@
 function project \
   --description 'Shortcut from anywhere to a project directory' \
-  --argument-names {project}
+  --argument-names project
+
+  if not set -q REPO_HOME
+    echo 'REPO_HOME environment variable must be set to the directory where all projects are stored'
+    exit 1
+  end
 
   cd $REPO_HOME/$project
 end
