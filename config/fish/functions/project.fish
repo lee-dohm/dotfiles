f080961afbcc528590fb2ff4c8af712efd57d685
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-03-24 15:01:51 -0700

    Allow project to switch to the repo home with no parameters

--- config/fish/functions/project.fish
+++ config/fish/functions/project.fish
@@ -1,11 +1,20 @@
 function project \
-  --description 'Shortcut from anywhere to a project directory' \
-  --argument-names project
+  --description 'Shortcut from anywhere to a project directory'
 
   if not set -q REPO_HOME
     echo 'REPO_HOME environment variable must be set to the directory where all projects are stored'
-    exit 1
+    return 1
   end
 
-  cd $REPO_HOME/$project
+  set arg_count (count $argv)
+
+  switch $arg_count
+  case 0
+    cd $REPO_HOME
+  case 1
+    cd $REPO_HOME/$argv[1]
+  case '*'
+    echo "You can only specify one project directory, you specified $arg_count"
+    return 2
+  end
 end
