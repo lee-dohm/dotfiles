e309969b0df50ec04d1318602b7f5cf9f0e4f309
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-04-16 09:56:23 -0700

    Only configure Ruby if rbenv is installed

--- config/fish/init/ruby.fish
+++ config/fish/init/ruby.fish
@@ -1,4 +1,6 @@
-status --is-interactive; and source (rbenv init -|psub)
+if program-exists rbenv
+  status --is-interactive; and source (rbenv init -|psub)
 
-alias b='bundle'
-alias be='bundle exec'
+  alias b='bundle'
+  alias be='bundle exec'
+end
