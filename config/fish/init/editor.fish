0ffd97f4ed308c4c2678c9dbe9bbd907c527a4d4
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-07-19 10:52:17 -0700

    Add Atom Nightly to Atom editor aliases

--- config/fish/init/editor.fish
+++ config/fish/init/editor.fish
@@ -5,6 +5,7 @@ set --export ATOM_DEV_RESOURCE_PATH ~/Source/atom/atom
 set --export ATOM_REPOS_HOME ~/Source
 
 alias a='atom'
-alias aa='atom-dev'
+alias aa='atom-nightly'
 alias ab='atom-beta'
 alias ad='atom-dev'
+alias an='atom-nightly'
