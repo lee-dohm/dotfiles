516c0e149d53b34ea2bf7f4c4a58685190773d01
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-07-19 10:50:49 -0700

    Use Anonymous Pro for text font

--- hyper.js
+++ hyper.js
@@ -9,10 +9,10 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 18,
 
     // font family with optional fallbacks
-    fontFamily: '"SFMono-Light", "Anka/Coder", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Anonymous Pro", "SFMono-Light", "Anka/Coder", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
