7a6a66d558b4550426ef01f68c0dfc0a201a33d6
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-07-17 15:51:11 -0700

    Remove redundant option

--- config/fish/functions/la.fish
+++ config/fish/functions/la.fish
@@ -1,3 +1,3 @@
 function la --description 'List contents of directory, including hidden files in directory using long format'
-  exa --all --all --binary --git --group --header --long $argv
+  exa --all --binary --git --group --header --long $argv
 end
