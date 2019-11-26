de7f9a789906324ddd005b6fb0f761b62e170443
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-11-26 15:33:03 -0800

    Standardize on double-underscore for private functions

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,24 +1,24 @@
-function _prompt_dir_component
-  _prompt_wrap_text (prompt_pwd) brblue
+function __prompt_dir_component
+  __prompt_wrap_text (prompt_pwd) brblue
 end
 
-function _prompt_err_component --argument-names {error_code}
+function __prompt_err_component --argument-names {error_code}
   if [ $error_code -ne 0 ]
-    _prompt_wrap_text $error_code brred
+    __prompt_wrap_text $error_code brred
   end
 end
 
-function _prompt_git_has_head
+function __prompt_git_has_head
   git rev-parse HEAD >/dev/null ^&1
 end
 
-function _prompt_git_component_no_commits
+function __prompt_git_component_no_commits
   # Display "no commits yet" in red when the repository has just been initialized but no initial
   # commit has been made
-  _prompt_wrap_text 'no commits yet' brred
+  __prompt_wrap_text 'no commits yet' brred
 end
 
-function _prompt_git_component_with_commits
+function __prompt_git_component_with_commits
   set --local sha (git rev-parse HEAD)
   set --local branch (git rev-parse --abbrev-ref HEAD)
 
@@ -37,27 +37,27 @@ function _prompt_git_component_with_commits
   switch $info[1]
   case branch
     # Display the branch name in green when we're on a branch
-    _prompt_wrap_text $info[2] green
+    __prompt_wrap_text $info[2] green
   case tag
     # Display the tag name in yellow when we're not on a branch but on a tag
-    _prompt_wrap_text $info[2] yellow
+    __prompt_wrap_text $info[2] yellow
   case detached
     # Display the SHA in red when we're not on a branch or a tag
-    _prompt_wrap_text (git rev-parse --short $sha) brred
+    __prompt_wrap_text (git rev-parse --short $sha) brred
   end
 end
 
-function _prompt_git_component
+function __prompt_git_component
   if __fish_is_git_repository
-    if _prompt_git_has_head
-      _prompt_git_component_with_commits
+    if __prompt_git_has_head
+      __prompt_git_component_with_commits
     else
-      _prompt_git_component_no_commits
+      __prompt_git_component_no_commits
     end
   end
 end
 
-function _prompt_wrap_text --argument-names {text,color}
+function __prompt_wrap_text --argument-names {text,color}
   set_color normal
   echo -n "["
   set_color --bold $color
@@ -69,8 +69,8 @@ end
 function fish_prompt
   set --local error_code $status
 
-  echo -n (_prompt_git_component)
-  echo -n (_prompt_dir_component)
-  echo -n (_prompt_err_component $error_code)
+  echo -n (__prompt_git_component)
+  echo -n (__prompt_dir_component)
+  echo -n (__prompt_err_component $error_code)
   echo ' '
 end
