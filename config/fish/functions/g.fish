50549cba13880ac6359dd53d42c90101ebafdb7f
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-11-26 15:32:33 -0800

    Use fallback function to simplify git alias again

--- config/fish/functions/g.fish
+++ config/fish/functions/g.fish
@@ -1,7 +1,9 @@
 function g -d "Alias for git: use hub, if available, and defaults to short status when no args are given" --wraps git
-  if [ (count $argv) -gt 0 ]
-    command -sq hub; and command hub $argv; or command git $argv
+  set --local args (fallback $argv "st")
+
+  if command -sq hub
+    command hub $args
   else
-    command -sq hub; and command hub st; or command git st
+    command git $args
   end
 end
