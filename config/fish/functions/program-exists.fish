93610daecc74868b145bdc00a3c98874c8d2795a
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-11-26 16:12:26 -0800

    :art:

--- config/fish/functions/program-exists.fish
+++ config/fish/functions/program-exists.fish
@@ -1,3 +1,6 @@
-function program-exists --argument-names {program} -d "Returns true if the command or program exists, false otherwise"
+function program-exists \
+  --argument-names {program} \
+  -d 'Returns true if the command or program exists, false otherwise'
+
   command -sq $program
 end
