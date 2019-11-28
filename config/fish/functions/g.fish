4937005f5106ecae5f71e21508575bc6d735f4e8
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-11-28 14:24:07 -0800

    Revert "Use fallback function to simplify git alias again"
    This reverts commit 50549cba13880ac6359dd53d42c90101ebafdb7f.

--- config/fish/functions/g.fish
+++ config/fish/functions/g.fish
@@ -1,9 +1,7 @@
 function g -d "Alias for git: use hub, if available, and defaults to short status when no args are given" --wraps git
-  set --local args (fallback $argv "st")
-
-  if command -sq hub
-    command hub $args
+  if [ (count $argv) -gt 0 ]
+    command -sq hub; and command hub $argv; or command git $argv
   else
-    command git $args
+    command -sq hub; and command hub st; or command git st
   end
 end
