96874289dd065f20eadce39733d80caac768c884
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-11-30 10:05:45 -0800

    Fix apm function for stable channel

--- config/fish/functions/apm.fish
+++ config/fish/functions/apm.fish
@@ -1,15 +1,15 @@
 function apm --description 'Allows any installed apm channel to be called using `apm`'
-  set --local apm_command ''
-
   if program-exists apm
-    set apm_command apm
-  else if program-exists apm-beta
-    set apm_command apm-beta
+    command apm $argv
   else
-    set apm_command apm-nightly
+    set --local apm ''
+
+    if program-exists apm-beta
+      set apm apm-beta
+    else
+      set apm apm-nightly
+    end
+
+    $apm $argv
   end
-
-  set --local exec_command $apm_command (string escape -- $argv)
-
-  eval "$exec_command"
 end
