fadfdc6b6aeeed9ac9d01487d9b597d6ddcbc936
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-12-12 10:59:49 -0800

    Use long option names

--- config/fish/functions/program-exists.fish
+++ config/fish/functions/program-exists.fish
@@ -2,5 +2,5 @@ function program-exists \
   --argument-names {program} \
   --description 'Returns true if the command or program exists, false otherwise'
 
-  command -sq $program
+  command --quiet --search $program
 end
