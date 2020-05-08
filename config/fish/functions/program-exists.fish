b3483ef901389aaa2a043d8d3123b6a3a8096f1d
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2020-05-08 15:18:28 -0700

    Remove deprecated array syntax

--- config/fish/functions/program-exists.fish
+++ config/fish/functions/program-exists.fish
@@ -1,5 +1,5 @@
 function program-exists \
-  --argument-names {program} \
+  --argument-names program \
   --description 'Returns true if the command or program exists, false otherwise'
 
   command --quiet --search $program
