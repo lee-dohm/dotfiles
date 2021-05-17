96397e5f05700b17a574b1a91e1b834182a8e0c0
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2021-05-17 14:10:29 -0700

    Use long form options

--- config/fish/functions/describe.fish
+++ config/fish/functions/describe.fish
@@ -3,8 +3,9 @@
 # https://github.com/ttscoff/fish_files/blob/master/functions/describe.fish
 #
 
-function describe -d 'Show description for function'
-  set -l d (functions -Dv $argv)
+function describe --description 'Show description for function'
+  set --local d (functions --details --verbose $argv)
+
   echo (set_color --dim brblue)"$d[1]:$d[3] ($d[2])"(set_color normal)
   echo (set_color --bold white)"> $d[5]"(set_color normal)
 end
