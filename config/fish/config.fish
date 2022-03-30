5d3331efcce857679b5b9fbad033970a4480addb
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2022-03-30 13:33:10 -0700

    Initialize Homebrew properly on both Intel and ASi Macs

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -19,7 +19,16 @@ end
 set --global fish_prompt_pwd_dir_length 0
 
 # Configure Homebrew before doing the other init scripts
-eval (/opt/homebrew/bin/brew shellenv)
+# /usr/local is used on Intel Macs
+# /opt/homebrew is used on Apple Silicon Macs
+if test -d /usr/local/bin/brew
+  eval (/usr/local/bin/brew shellenv)
+end
+
+if test -d /opt/homebrew/bin/brew
+  eval (/opt/homebrew/bin/brew shellenv)
+end
+
 
 for init in (command ls ~/.config/fish/init)
   source ~/.config/fish/init/$init
