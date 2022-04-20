1e0023aed9b2333c70a320e29a71c3ac601f27e2
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2022-04-19 20:42:49 -0700

    Change Hyper theme to hyper-named-css-colors

--- hyper.js
+++ hyper.js
@@ -119,7 +119,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ["hyperlinks", "hyper-one-dark", "verminal"],
+  plugins: ["hyperlinks", "hyper-named-css-colors"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
